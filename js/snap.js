5f54ddb3c8c9ce292ec55b348f657162d15da836
Shalom <shaladi@mit.edu>
2014-11-24 11:55:18 -0500

    Implemented socket protocols and basic gameplay

--- js/snap.js
+++ js/snap.js
@@ -1,4 +1,45 @@
+var socket =io('http://localhost:8080');
+var playerName = ''
+var score = 0
+
 $( "#nameForm" ).submit(function( event ) {
-  alert( "Handler for .submit() called." );
-  event.preventDefault();
+    playerName = $('#name').val();
+    socket.emit('new player', playerName);
 });
+
+$("#wordForm").submit(function(event){
+    var word = $('#word').val();
+    socket.emit('new word', word);
+    $('#wordList').append('<span>'+word.toLowerCase()+'</span><br>');
+    $('#word').val('');
+    $("#wordList").animate({ scrollTop: $("#wordList").prop("scrollHeight")}, 100);
+});
+
+socket.on('snap', function(data) {
+   score = score + data.d_score
+   $('#wordList').html()
+   $('#wordList').append('<span class="snappedWord">'+data.word+' ('+data.player+')</span><br>');
+   $('#score').html(score);
+});
+
+socket.on('game over', function(data){
+   $('#word').prop('disabled', true);
+});
+
+socket.on('user joined', function(data) {
+    if (data.player === playerName){
+        enterGameState();
+    }
+});
+
+socket.on('game started', function(data) {
+    //start client timer here
+    $('#word').removeAttr('disabled');
+});
+
+function enterGameState() {
+    $( "#nameForm" ).fadeOut( "slow", function() {
+        $('#gameState').css('visibility', 'visible');
+        $('#gameState').css('height','auto');
+    });
+}
